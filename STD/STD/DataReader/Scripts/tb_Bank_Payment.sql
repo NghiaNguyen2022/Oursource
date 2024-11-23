93efc98fc9a828e8d8dbbd77a0dddcd9df3fe6bd
nghianguyen <nghia.nhan.swe@gmail.com>
2024-11-23 11:17:48 +0700

    a

---
+++ STD/STD/DataReader/Scripts/tb_Bank_Payment.sql
@@ -0,0 +1,50 @@
+drop TABLE "tb_Bank_Payment"
+CREATE TABLE "tb_Bank_Payment"
+(
+	"CardCode" VARCHAR(50),
+	"DocEntry" INT,
+	"PaymentType" CHAR(1),
+	"PaymentKey" VARCHAR(50),
+	"Status"  VARCHAR(10),
+	"StatusName" VARCHAR(500),
+	"DateAction" Date,
+	"Key" VARCHAR(30)
+)
+
+-- Call "SP_LogPaymentTool" ('NC0100051', '1283', 'C', 'F', 'Không thể tạo phiếu thanh toán nhiều hóa đơn khác nhau về Dòng tiền.') 
+
+CREATE PROCEDURE "usp_Bank_LogPaymentTool"
+(
+	IN v_CardCode NVARCHAR(100),
+	IN v_DocEntry INT,
+	IN v_DocType CHAR(1),
+	IN v_PaymentKey VARCHAR(50),
+	IN v_Status VARCHAR(1),
+	IN v_StatusName VARCHAR(500),
+	IN v_key VARCHAR(30)
+	
+)
+LANGUAGE SQLSCRIPT
+AS
+BEGIN
+	IF EXISTS (SELECT 1	
+				 FROM "tb_Bank_Payment"
+				WHERE "CardCode" = :v_CardCode
+				  AND "DocEntry" = :v_DocEntry
+				  AND "PaymentType" = :v_DocType
+				)
+	THEN
+		UPDATE "tb_Bank_Payment"
+ 		   SET "PaymentKey" = :v_PaymentKey,
+ 		   	   "Status" = :v_Status,
+ 		   	   "StatusName" = :v_StatusName,
+ 		   	   "DateAction" = CURRENT_DATE,
+ 		   	   "Key" = :v_key
+ 		 WHERE "CardCode" = :v_CardCode
+		   AND "DocEntry" = :v_DocEntry
+		   AND "PaymentType" = :v_DocType;
+	ELSE
+		INSERT INTO "tb_Bank_Payment"
+		VALUES (:v_CardCode, :v_DocEntry, :v_DocType, :v_PaymentKey, :v_Status, :v_StatusName, CURRENT_DATE, :v_key);
+	END IF;
+END;
---
+++ STD/STD/DataReader/Scripts/usp_Bank_APInvoicesToPayment.sql
@@ -0,0 +1,74 @@
+-- Call "usp_InvoicesToPayment" ('20240101', '20240415', 'C', '1')
+CREATE PROCEDURE "usp_Bank_APInvoicesToPayment"
+(
+	IN v_FromDate DATE,
+	IN v_ToDate DATE,
+	IN v_Type CHAR(2),
+	IN v_Account NVARCHAR(50) DEFAULT 'All',
+	IN v_FeeType NVARCHAR(50) DEFAULT '', 	
+	IN v_Key VARCHAR(100)  DEFAULT ''
+)
+LANGUAGE SQLSCRIPT
+AS
+BEGIN
+	IF :v_Type = 'PC' -- chi
+	THEN
+		BEGIN 
+			SELECT T."CardCode",
+				    T."CardName",
+				    T."Check",
+				    T."DocEntry" AS "DocNum",
+				    T."DocDate", 
+				    T."DueDate",
+				    t."JrnlMemo",
+				    T."DocCur",
+				    T."InsTotal" AS "InsTotal",	
+					T."InsTotalFC",	    
+				    CAST(T."MustPay" AS DECIMAL) AS "MustPay",  
+				    T."DocEntry",
+				    CAST('' AS NVARCHAR(200))  AS "CFlow",
+				    'Data' AS "Manual"
+			   FROM (
+					SELECT T."CardCode",
+						    T."CardName",
+						    T."Check",
+						    T."DocNum",
+						    T."DocDate", 
+						    T."DueDate",
+						    T."DocCur",
+						    T."InsTotal" AS "InsTotal",		  
+						    T."InsTotalFC",
+	  				        T."DocRate",		      
+						    CASE WHEN T."MustPay" > 0 THEN T."MustPay"
+						   	 	 ELSE 0 
+						   	 END AS "MustPay",  
+						    T."DocEntry",
+					        t."JrnlMemo",
+						    'Data' AS "Manual"
+					  FROM (SELECT T0."CardCode",
+								   T2."CardName",
+								   'N' AS "Check",
+								   T0."DocNum",
+								   T0."DocDate", 
+								   T1."DueDate",
+								   T1."InsTotal",
+								   T1."InsTotalFC",
+								   CASE WHEN T0."DocCur" = 'VND' THEN T1."InsTotal" - T1."PaidToDate" 
+								   		ELSE T1."InsTotalFC" - T1."PaidFC" 
+								   	END AS "MustPay",
+								   T0."DocCur",
+								   T0."DocEntry",
+							      t0."JrnlMemo"
+							  FROM OPCH T0
+							  LEFT JOIN PCH6 T1 ON T0."DocEntry" = T1."DocEntry"
+							  LEFT JOIN OCRD T2 ON T0."CardCode" = T2."CardCode"	
+							 WHERE T0."DueDate" BETWEEN :v_FromDate AND :v_ToDate
+							   AND T1."InsTotal" - T1."PaidToDate" > 0		
+							   AND (:v_Account = 'All' OR t2."HousBnkAct" = :v_Account)
+						     ORDER BY T0."CardCode"
+							) T
+				 )T;
+			
+			END;
+		END IF;
+END;
