ade953d3e146e892fe9e08dd475e706ad7f0a7fc
nghianguyen <nghia.nhan.swe@gmail.com>
2024-12-02 09:06:12 +0700

    ngia nguyen

---
+++ API/PN.ApplicationAPI/PN.ApplicationAPI/DataReader/Scripts/tb_Bank_BIDV_Accesstoken.sql
@@ -0,0 +1,24 @@
+﻿DROP TABLE "tb_Bank_BIDV_AccesstokenINT"
+
+CREATE TABLE "tb_Bank_BIDV_AccesstokenINT"
+(
+	"PaymentMethod"	INT,
+	"PaymentMethodName"	NVARCHAR(50),
+	"PurDate" NVARCHAR(20),
+	"MerchantUserName" NVARCHAR(50),
+	"ShopId" NUMERIC,
+	"MasterShopID" NUMERIC,
+	"OrderNo" NVARCHAR(50),
+	"OrderCash" DECIMAL(19,2),
+	"BankName" NVARCHAR(50),
+	"CardNumber" NVARCHAR(50),
+	"CardIssuanceType" INT,
+	"PaymentStatus" INT,
+	"MDD1" NVARCHAR(50),
+	"MDD2" NVARCHAR(50),
+	"PaymentSource" NVARCHAR(50),
+	"VoucherTotalAmount" DECIMAL(19,2),
+	"VoucherID" NVARCHAR(50),
+	"IntDate" DATE,
+	"IntTime" NVARCHAR(6) -- 'HHmmss'
+)
