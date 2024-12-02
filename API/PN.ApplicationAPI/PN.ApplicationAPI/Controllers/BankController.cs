048bd035923c327695178ffead4ae724e271dfa3
Nghia Nguyen <nghia.nhan.swe@gmail.com>
2024-12-02 21:50:48 +0700

    nghia nguyen

--- API/PN.ApplicationAPI/PN.ApplicationAPI/Controllers/BankController.cs
+++ API/PN.ApplicationAPI/PN.ApplicationAPI/Controllers/BankController.cs
@@ -1,5 +1,8 @@
 ﻿using APICore.Authen.Atts;
 using PN.ApplicationAPI.APICore.Controller;
+using PN.ApplicationAPI.Models;
+using SAPCore.Config;
+using STD.DataReader;
 using System.Web.Http;
 
 namespace PN.ApplicationAPI.Controllers
@@ -8,19 +11,19 @@ namespace PN.ApplicationAPI.Controllers
     {
         [BasicAuthentication]
         [System.Web.Http.HttpPost]
-        public IHttpActionResult Create([FromBody]Payoo_Payment input)
+        public IHttpActionResult Create([FromBody]BIDV_Accesstoken input)
         {
             if (input == null)
             {
                 return Ok(ResponseFaild("Data chưa đúng cấu trúc và định dạng"));
             }
 
-            var query = string.Format(QueryString.CheckOrderExists, input.OrderNo);
-            var data = DataProvider.QuerySingle(CoreSetting.DataConnection, query);
-            if (data != null && data["Existed"].ToString() == "Existed")
-            {
-                return Ok(ResponseFaild("Order No đã tồn tại"));
-            }
+            //var query = string.Format(QueryString.CheckOrderExists, input.OrderNo);
+            //var data = DataProvider.QuerySingle(CoreSetting.DataConnection, query);
+            //if (data != null && data["Existed"].ToString() == "Existed")
+            //{
+            //    return Ok(ResponseFaild("Order No đã tồn tại"));
+            //}
             var message = string.Empty;
             if (input.InsertData(ref message))
                 return Ok(ResponseSuccessed(message));
--- API/PN.ApplicationAPI/PN.ApplicationAPI/Models/BIDV_Accesstoken.cs
+++ API/PN.ApplicationAPI/PN.ApplicationAPI/Models/BIDV_Accesstoken.cs
@@ -1,7 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using SAPCore.Config;
+using STD.DataReader;
+using System;
+using System.Configuration;
 
 namespace PN.ApplicationAPI.Models
 {
@@ -9,5 +9,35 @@ namespace PN.ApplicationAPI.Models
     {
         public string Code { get; set; }
         //public string Date { get; set; }
+        public bool InsertData(ref string message)
+        {
+            //using(var trans)
+            try
+            {
+                var dbName = CoreSetting.System == SystemType.SAP_HANA ?
+                    ConfigurationManager.AppSettings["Schema"] + "\"." : ConfigurationManager.AppSettings["Schema"] + "\"..";
+                var query = "INSERT INTO \"" + dbName + "\"tb_Bank_BIDV_AccesstokenINT\" VALUES ( ";
+                query += $"'{Code}',";
+                query += $"'{DateTime.Now.ToString("yyyyMMdd")}',";
+                query += $"'{DateTime.Now.ToString("HHmmss")}'";
+                query += ")";
+
+                var ret1 = dbProvider.ExecuteNonQuery(query);
+                if (ret1 == 1)
+                {
+                    message = "Lưu thành công";
+                }
+                else
+                {
+                    message = "Lưu thất bại";
+                }
+                return ret1 == 1;
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return false;
+        }
     }
 }
